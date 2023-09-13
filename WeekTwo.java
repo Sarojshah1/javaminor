891c464e1f74ca12b389371a5d1f5bd9b33e0ba9
saroj kumar sah <sarojahah152@gmail.com>
2023-09-13 11:19:44 +0545

    weektow

---
+++ WeekTwo.java
@@ -0,0 +1,39 @@
+// 1. Write a program to check whether a person can cast a vote or not. The condition is you must be over 18 years old to vote.
+import java.util.Scanner;
+public class WeekTwo {
+    public static void main(String[] args) {
+        // Scanner age = new Scanner(System.in);
+        // System.out.println("enter you age");
+        // int a=age.nextInt();
+
+        // String b=(a>18)? "you can vote":"you cannot vote";
+        // System.out.println(b);
+
+        // 3. Write a program to calculate, area of the triangle, and the Volume of the Cube and Cuboid.
+        Scanner area = new Scanner(System.in);
+        // System.out.println("enter base of triangle");
+        // int  b=area.nextInt();
+        // System.out.println("enter height of triangle");
+        // int h=area.nextInt();
+        // int a;
+        // a=(b*h)/2;
+        // System.out.println("area of triangle is "+a); 
+        // System.out.println("enter the length of cube");
+        // float l=area.nextFloat();
+        // float cube;
+        // cube=l*l*l;
+        // System.out.printf("volume of cube is %.2f",cube);
+        // System.out.println("enter the length of cuboid");
+        // float l=area.nextFloat();
+        // System.out.println("enter the width of cuboid");
+        // float w=area.nextFloat();
+        // System.out.println("enter the height of cuboid");
+        // float h=area.nextFloat();
+        // float cuboid=l*w*h;
+        // System.out.println("the volume of cuboid is "+cuboid);
+        
+
+        
+    }
+    
+}
