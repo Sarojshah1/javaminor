9c4a8938810a30aece105b2b7776d10b51231133
saroj kumar sah <sarojahah152@gmail.com>
2023-09-13 11:20:48 +0545

    weektow

---
+++ hello.java
@@ -0,0 +1,13 @@
+public class hello{
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("*****");
+        System.out.println("*****");
+        System.out.println("*****");        
+        System.out.println("\t    *");        
+        System.out.println("\t   * *");        
+        System.out.println("\t  * * *");        
+        System.out.println("\t * * * *");        
+
+    }
+} 
