387fcbe2f350724b9b0ca4e7567488ac9cbd1cdc
saroj kumar sah <sarojahah152@gmail.com>
2023-09-19 13:07:52 +0545

    change

---
+++ ConditionSwithCase.java
@@ -0,0 +1,13 @@
+public class ConditionSwithCase {
+    public static void main(String[] args) {
+        int exp=10;
+        switch (exp) {
+            case 10:
+                System.out.println("Expected");
+                break;
+            }
+
+        
+    }
+    
+}
