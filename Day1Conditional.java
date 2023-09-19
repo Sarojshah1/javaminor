6d34cf36131a77ab63559c9dc4f5cf295c1a7ff3
saroj kumar sah <sarojahah152@gmail.com>
2023-09-19 12:28:11 +0545

    conditional part

---
+++ Day1Conditional.java
@@ -0,0 +1,17 @@
+public class Day1Conditional {
+    public static void main(String[] args) {
+        int a=3;
+        if (a==10){
+            System.out.println("Statement  1 is true");
+        }
+        else if (a<5){
+            System.out.println("Statement 2 is false");
+        }else if (a<10){
+            System.out.println("Statement 3 is false");
+        }
+        else{
+            System.out.println("statement is not true");    
+        }
+    }
+    
+}
