385fa8c9081731b4316e7eac64c2683b24b6451b
saroj kumar sah <sarojahah152@gmail.com>
2023-09-20 11:10:52 +0545

    change

--- ConditionSwithCase.java
+++ ConditionSwithCase.java
@@ -1,15 +1,34 @@
+import java.util.Scanner;
+
 public class ConditionSwithCase {
     public static void main(String[] args) {
-        int exp=10;
-        switch (exp) {
-            case 10:
-                System.out.println("Expected");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter  a number: 1.sunday,2.monday,3.tuesday,4.wednesday,5.thursday,6.friday,7.Saturday");
+        int n = sc.nextInt();
+        switch(n){
+           case 1:
+                System.out.println("Sunday");
                 break;
-            case 20:
-                System.out.println(" not Expected");
+            case 2:
+                System.out.println("Monday");
+                break;
+            case 3:
+                System.out.println("tuesday");
+                break;
+            case 4:
+                System.out.println("wednesday");
+                break;
+            case 5:
+                System.out.println("thursday");
+                break;
+            case 6:
+                System.out.println("friday");
+                break;
+            case 7:
+                System.out.println("saturday");
                 break;
 
-            }
+
 
         
     }
---
+++ LoopPart.java
@@ -0,0 +1,6 @@
+public class LoopPart {
+    public static void main(String[] args) {
+        
+    }
+    
+}
