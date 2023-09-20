0f322f58ad6fd6aa5f1c0c3e362a91949a7a39cc
saroj kumar sah <sarojahah152@gmail.com>
2023-09-20 11:29:44 +0545

    change

--- LoopPart.java
+++ LoopPart.java
@@ -1,6 +1,13 @@
 public class LoopPart {
     public static void main(String[] args) {
-        
+        for (int i=1;i<=10;i++ ) {
+            System.out.println("#######################Multiplication table of"+i+"####################");
+
+            for (int j=1;j<=10;j++) {
+                
+            System.out.println(i+ "* "+j+"="+i*j);
+            }
+        }
     }
     
 }
