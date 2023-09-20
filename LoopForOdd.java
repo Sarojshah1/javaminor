e9140a0ff3fcb5267583b72f9102c5eceda5f9dc
saroj kumar sah <sarojahah152@gmail.com>
2023-09-20 11:36:09 +0545

    change

---
+++ LoopForOdd.java
@@ -0,0 +1,12 @@
+public class LoopForOdd {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i + " is even");
+            } else {
+                System.out.println(i + " is odd");
+            }
+        }
+    }
+
+}
