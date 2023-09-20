6189492bcecb8ebd89f091bea0fd776b000cc1b5
saroj kumar sah <sarojahah152@gmail.com>
2023-09-20 11:40:11 +0545

    change

--- LoopForOdd.java
+++ LoopForOdd.java
@@ -1,12 +1,22 @@
+import java.util.Scanner;
+
 public class LoopForOdd {
     public static void main(String[] args) {
-        for (int i = 1; i <= 10; i++) {
-            if (i % 2 == 0) {
-                System.out.println(i + " is even");
-            } else {
-                System.out.println(i + " is odd");
-            }
+        // for (int i = 1; i <= 10; i++) {
+        //     if (i % 2 == 0) {
+        //         System.out.println(i + " is even");
+        //     } else {
+        //         System.out.println(i + " is odd");
+        //     }
+        // }
+        Scanner sc =new Scanner(System.in);
+        System.out.println("enter a number");
+        int n=sc.nextInt();
+        for (int i=1; i <= n; i++) {
+            System.out.println("Square of "+i+" is "+i*i);
+
         }
+        sc.close();
     }
 
 }
