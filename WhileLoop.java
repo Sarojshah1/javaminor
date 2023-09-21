27e3f2a7883fcb68990e88c1cfdf4000b1d7850c
saroj kumar sah <sarojahah152@gmail.com>
2023-09-21 12:55:37 +0545

    change

--- WhileLoop.java
+++ WhileLoop.java
@@ -1,9 +1,33 @@
+import java.util.Scanner;
+
 public class WhileLoop {
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
-        int count = 0;
-        while (count <=10) {
-            System.out.println(count++);
+        int count = 10;
+        // while (count <=10) {
+        //     System.out.println("2 * " + count + "="+2*count ++ );
+        // }
+        // Scanner sc=new Scanner(System.in);
+        // int val=sc.nextInt();
+        // while(val<10||val>100){
+        //     System.out.println("the number is invalid");
+        //     val=sc.nextInt();
+        // }
+        // sc.close();
+
+        // break statement part
+        for(int i=1; i<count; i++){
+            if(i%2!=0){
+                continue;
+                // System.out.println("exit"); 
+                 
+            }
+            System.out.println(i+" number is even");
+        
         }
+        
     }
     
 }
