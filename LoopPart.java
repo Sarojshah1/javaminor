63e6bc931066f0c980bdba054f14cffc62f307a9
saroj kumar sah <sarojahah152@gmail.com>
2023-09-21 12:15:46 +0545

    change

--- LoopPart.java
+++ LoopPart.java
@@ -1,12 +1,18 @@
 public class LoopPart {
     public static void main(String[] args) {
-        for (int i=1;i<=10;i++ ) {
-            System.out.println("#######################Multiplication table of"+i+"####################");
+        // for (int i=1;i<=10;i++ ) {
+        //     System.out.println("#######################Multiplication table of"+i+"####################");
 
-            for (int j=1;j<=10;j++) {
+        //     for (int j=1;j<=10;j++) {
                 
-            System.out.println(i+ "* "+j+"="+i*j );
+        //     System.out.println(i+ "* "+j+"="+i*j);
+        //     }
+        // }
+        for (int i=1;i<=7;i++) {
+            for (int j=7-i;j>=1;j--) {
+                System.out.print("* ");
             }
+            System.out.println( );
         }
     }
     
---
+++ WhileLoop.java
@@ -0,0 +1,9 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        int count = 0;
+        while (count <=10) {
+            System.out.println(count++);
+        }
+    }
+    
+}
